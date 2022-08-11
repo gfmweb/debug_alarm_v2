c2cb4b4958e2699de3d82f26caea4f7f0b614209
gfm <gfmwebpost@gmail.com>
2022-08-11 14:25:48 +0500

    Оповещение пользователя о том что подвязан его ID

--- app/Controllers/TELEGRAM/TelegramHooks.php
+++ app/Controllers/TELEGRAM/TelegramHooks.php
@@ -12,9 +12,7 @@ class TelegramHooks extends BaseController
     {
 	    $data = file_get_contents('php://input');
 	    $data = json_decode($data, true);
-		$file = fopen('hook.txt','w');
 		if(!isset($data['message']['from']['id'])||!isset($data['message']['text'])){
-			
 			die();
 		}
 		$sender = $data['message']['from']['id'];
@@ -22,22 +20,18 @@ class TelegramHooks extends BaseController
 	    $findme   = '/start';
 	    $pos = strpos($text, $findme);
 	    if ($pos === false) {
-		   
 		    die();
 	    }
 		else // Если deepLink удовлетворяет условиям
 		{
-			
 		   $text = trim(str_replace('/start','',$text));
 		   $text = base64_decode($text);
-			
 		   if($text =='create_admin'){ //Регистрация первого админа (добавление его пользователю Телеграм ID)
-			  
 			   $Users = model(UserModel::class);
 			   $pretendent = $Users->find(1);
-			   
 			   if(is_null($pretendent['user_telegram_id'])) { // Проверяем что у пользователя 1 еще нет телеграмID
 				   $Users->completeRegisterUser(1, (int)$sender);
+				   TelegramAPI::sendMessage((int)$sender,'<b>Регистрация почти завершена'.PHP_EOL.'Зайдите в админку</b>',false);
 			   }
 		   }
 		   $user_array = explode('create_user/id=',$text);
@@ -46,9 +40,9 @@ class TelegramHooks extends BaseController
 			   $pretendent = $Users->find((int)$user_array[1]);
 			   if(is_null($pretendent['user_telegram_id'])) { // Проверяем что у пользователя еще нет телеграмID
 				   $Users->completeRegisterUser((int)$user_array[1], (int)$sender);
+				   TelegramAPI::sendMessage((int)$sender,'<b>Регистрация завершена'.PHP_EOL.'Зайдите в лк на сайте</b>',false);
 			   }
 		   }
 	    }
-		
     }
 }
